3edf32e80e58d81ed0b370e9f1376bde5d9475d6
anonREAuthor <anonREAuthor@gmail.com>
2015-04-18 17:00:04 +1000

    Initial Commit of Spatial Econometric Tools
    A set of tools for working with spatial econometric models in R

---
+++ spatEconTools.R
@@ -0,0 +1,35 @@
+
+### Function for calculating the Pseudo R2 of a spatial error model ----------------------
+
+calcPseudoR2 <- function(semModel){
+  pR2 <- (1 - (as.numeric(semModel$SSE) / 
+                 sum((semModel$y - mean(semModel$y)) ^ 2)))
+  return(pR2)
+}
+
+### Function for building a spatial weights matrix (NOT COMPLETE)
+
+createSWM <- function(data # SpatialPointsDataFrame
+                      ,knn  # Number of neighbors desired
+                      ,distWeighted = TRUE # Distance Weighted ?
+                      ,nugget = 1 # How much to add to identical points
+                      ){
+  
+  ## Create distance weighting function
+  
+  dwf <- function(x) {1 / ((x + nugget) ^ 2)}
+  
+  ## Create Neighbor List
+  nbList <- knn2nb(knearneigh(data, knn))
+  
+  ## Create Distances
+  if(distWeighted){
+    nbDists <- nbdists(nbList, data)    
+  }
+  
+  ## Building Weights Matrix
+  swm <- listw2U(nb2listw(nbList, glist = lapply(nbDists, dwf)
+                               , style="W",zero.policy=T))    
+}
+  
+  
